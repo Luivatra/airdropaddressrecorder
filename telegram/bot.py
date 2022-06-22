86d049d592407683e5421842bd560366faf075f1
Luivatra <luivatra@gmail.com>
2022-06-22 14:51:52 +0200

    tosi telegram

--- telegram/bot.py
+++ telegram/bot.py
@@ -14,7 +14,7 @@ updater = Updater(token=os.environ.get("TELEGRAM_TOKEN"))
 def register(update: Update, context: CallbackContext):
     user: User = update.message.from_user
     if user.id != update.effective_chat.id:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please contact me in DM only so we dont spam this chat! Go to @anetabtcairdroprecorderbot")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please contact me in DM only so we dont spam this chat! Go to @tosiaddressrecorderbot")
         return
     if context.args.__len__() < 1:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Please supply an Ergo address as the first argument to the command. (Like this: /register 9myergoadressjjjsz")
@@ -41,8 +41,8 @@ def register(update: Update, context: CallbackContext):
                 where join_date <= (select join_date from telegram_users where user_id = '%s')
                 """,(user.id,))
                 ogcount = cur.fetchone()[0]
-                if ogcount > 0 and ogcount <= 1500:
-                    extra = f"You were Telegram member number {ogcount}, congratulations and thank you for being one of the first 1,500 Telegram members to join our community. 😇 You are now successfully registered and will receive your airdrop soon!🥳🎉"
+                if ogcount > 0 and ogcount <= 3000:
+                    extra = f"You were Telegram member number {ogcount}, congratulations and thank you for being one of the first 3,000 Telegram members to join our community. 😇 You are now successfully registered and will receive your airdrop soon!🥳🎉"
                 context.bot.send_message(chat_id=update.effective_chat.id, text=f"CONGRATULATIONS! 🎊 You successfully registered your Ergo Wallet address. {extra}")
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text="ERROR! Please re-enter a valid Ergo wallet address.")
